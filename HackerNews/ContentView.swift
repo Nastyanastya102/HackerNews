f9e8eaf0c322ba3f1c7f088efda5d77103651cec
StacyKlyashtorna <Little.smart.bee@gmail.com>
2024-10-09 20:31:54 -0700

    Initial Commit

---
+++ HackerNews/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HackerNews
+//
+//  Created by Nastya Klyashtorna on 2024-10-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by Nastya Klyashtorna on 2024-10-09.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
